3b80e49e05b9ab1d2b92bf8a69671380f96912c8
BamJon <97885341+BamJon@users.noreply.github.com>
2025-01-29 12:35:10 +0000

    Update githubupdate.php

--- githubupdate.php
+++ githubupdate.php
@@ -1,67 +1,14 @@
-<?php
-/*
-Plugin Name: Your Custom Plugin
-Update URI: https://github.com/BamJon/githubupdate
-Version: 1.1
+<?php /*
+plugin: gitup test
+Theme URI: 
+Github Theme URI: https://github.com/BAMJon/githubupdate
+Description: A very basic theme for experimentation and learning
+Author: Dan York
+Author URI: http://danyork.com/
+Version: v1.2.3
+License:  GPL
+
+A very barebones theme purely for me to learn about WordPress Themes
 */
- 
-add_filter( 'update_plugins_github.com', 'self_update', 10, 4 );
 
-/**
- * Check for updates to this plugin
- *
- * @param array  $update   Array of update data.
- * @param array  $plugin_data Array of plugin data.
- * @param string $plugin_file Path to plugin file.
- * @param string $locales    Locale code.
- *
- * @return array|bool Array of update data or false if no update available.
- */
-function self_update( $update, array $plugin_data, string $plugin_file, $locales ) {
-
-	// only check this plugin
-	if ( 'githubupdate/githubupdate-plugin.php' !== $plugin_file ) {
-		return $update;
-	}
-
-	// already completed update check elsewhere
-	if ( ! empty( $update ) ) {
-		return $update;
-	}
-
-	// let's go get the latest version number from GitHub
-	$response = wp_remote_get(
-		'https://api.github.com/repos/your-org/your-custom-plugin/releases/latest',
-		array(
-			'user-agent' => 'BamJon',
-		)
-	);
-
-	if ( is_wp_error( $response ) ) {
-		return;
-	} else {
-		$output = json_decode( wp_remote_retrieve_body( $response ), true );
-	}
-
-	$new_version_number  = $output['tag_name'];
-	$is_update_available = version_compare( $plugin_data['Version'], $new_version_number, '<' );
-
-	if ( ! $is_update_available ) {
-		return false;
-	}
-
-	$new_url     = $output['html_url'];
-	$new_package = $output['assets'][0]['browser_download_url'];
-
-	error_log('$plugin_data: ' . print_r( $plugin_data, true ));
-	error_log('$new_version_number: ' . $new_version_number );
-	error_log('$new_url: ' . $new_url );
-	error_log('$new_package: ' . $new_package );
-
-	return array(
-		'slug'    => $plugin_data['TextDomain'],
-		'version' => $new_version_number,
-		'url'     => $new_url,
-		'package' => $new_package,
-	);
-}
+?>
